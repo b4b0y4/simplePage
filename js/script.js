da286173d52634f3ccc2be50691364bec4103c88
b4b0y4 <133552984+b4b0y4@users.noreply.github.com>
2024-02-12 21:30:39 +0900

    added blur when panel open

--- js/script.js
+++ js/script.js
@@ -2,13 +2,16 @@ document.addEventListener('DOMContentLoaded', function () {
     const burgerMenuButton = document.querySelector('.burger-menu-button')
     const panel = document.getElementById('panel')
     const closeButton = document.querySelector('.close-button')
+    const overlay = document.getElementById('overlay')
 
     burgerMenuButton.addEventListener('click', function () {
         panel.classList.toggle('open')
-    });
+        overlay.style.display = "block"
+    })
 
     closeButton.addEventListener('click', function () {
         panel.classList.remove('open')
+        overlay.style.display = "none"
     })
 })
 
